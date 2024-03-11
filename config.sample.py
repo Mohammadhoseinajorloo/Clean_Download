62158817bd2136a974b7b544c7a6fd9f1a9761f7
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-03-11 17:09:27 +0330

    ADD LOGGERS This commit writed move function code and add loggers class .gitingnor : ignored logs folder config.sample.py : add movies and pdf file path loggers.py : writed loggers class main.py : add move function writer: itnj-geek

--- config.sample.py
+++ config.sample.py
@@ -1,1 +1,3 @@
 download_path = "Please, set your download folder"
+movies_path = "Please, set your movies folder" 
+pdf_path = "Please, set your pdf folder"
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from config import *
 import shutil
 import os
 
-def check_extenstion(directory):
 
+info_logger = Loggers(logging.INFO, ('%(asctime)s, %(name)s, %(message)s'), "logs/info.log")
+
+warning_logger = Loggers(logging.WARNING, ('%(asctime)s, %(name)s, %(message)s'), "logs/warning.log")
+
+debug_logger = Loggers(logging.DEBUG, ('%(asctime)s, %(name)s, %(message)s'), "logs/debug.log")
+
+def check_extenstion(directory):
     extenstion = {}
 
     for root, dirs, files in os.walk(directory):
@@ -16,11 +22,47 @@ def check_extenstion(directory):
     return extenstion
 
 
+def moved(extension, format_type, movies_file):
+    for format_type in extension[str(format_type)]:
+        sutill.move(format_type, movies_file)
+        return info_logger.info(f"{format_type} Moved in {movies_file}")
+
+
 
 if __name__=="__main__":
+
     if not os.listdir(download_path):
-        print (f"Directory {download_path} is empty")
+        warning_logger.warning(f"Directory {download_path} is empty")
+
     else:
-        pass
+        try:
+
+            mp4_count = 0
+            mkv_count = 0
+            pdf_count = 0
+
+            extension = check_extenstion(download_path)
+            if extension["mp4"] in extension:
+                moved(extension, mp4 ,movies_path)
+                mp4_count += 1
+            else:
+                warning_logger.warning("mp4 file not exists")
+
+            if extension["mkv"] in extension:
+                moved(extension, mkv,movies_path)
+                mkv_count += 1
+            else:
+                warning_logger.warning("mkv file not exists")
+
+            if extension["pdf"] in extension:
+                moved(extension, pdf, pdf_path)
+                pdf_count += 1
+            else:
+                warning_logger.warning("pdf file not exists")
+
+        except:
+            debug_logger.debug("Please check download file")
+
+
 
 
